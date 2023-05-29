87c74c06f701a0888a35878b1319f47c3ac30e7b
AaronAnablon <anablonaaronp@gmail.com>
2023-05-30 02:29:11 +0800

    Added local and active links in the api

--- frontend/src/slices/api.js
+++ frontend/src/slices/api.js
@@ -2,6 +2,12 @@ export const url = "https://moon-delivery-api.onrender.com/api";
 
 export const server = "https://moon-delivery-api.onrender.com";
 
+// export const url = "http://localhost:5000/api";
+
+// export const server = "http://localhost:5000";
+
+
+
 export const setHeaders = () => {
   const headers = {
     headers: {
